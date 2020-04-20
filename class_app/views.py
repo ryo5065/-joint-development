0588aa50a7d715907dbf711a7af09540927ad7e0
上山良大 <ueyamaryoudai@ueyamaryoudainoMacBook-Pro.local>
2020-04-20 17:50:20 +0900

    create the top page function and move css and js files to static folder

--- class_app/views.py
+++ class_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import CreateView, TemplateView, ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import School
+from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
+class Top(TemplateView):
+    template_name = "top.html"
+
+def zoom(request,pk):
+    return render(request,'zoom_list.html') #ここはエラーが出ないように適当に書いてるだけなので乾くん後はよろしくです
+    
+
