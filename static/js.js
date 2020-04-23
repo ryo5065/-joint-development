c68a85c3df079f733af8b9ca03b18f145f68d0af
”Ryosuke <all19.ryo@gmail.com>
2020-04-23 15:15:29 +0900

    TOPをdjangoに適応させるために修正しました。

--- static/js.js
+++ static/js.js
@@ -8,10 +8,10 @@ $(function(){
     $('#school').vegas({
         
         slides: [
-            { src:'img/kuc.jpg' },
-        { src: 'img/kgc.jpg' },
-        { src: 'img/duc.jpg' },
-        { src:'img/ruc.jpg'}
+            { src:'../static/kuc.jpg' },
+        { src: '../static/kgc.jpg' },
+        { src: '../static/duc.jpg' },
+        { src:'../static/ruc.jpg'}
         ],
         delay: 2000,
         timer: false,
@@ -43,31 +43,31 @@ $('.nav_toggle').on('click', function () {
     });
 
     $('#ku-univ').mouseover(function(){
-        $(this).attr('src', 'img/door.png');
+        $(this).attr('src','static/door.png');
       });
       $('#ku-univ').mouseleave(function(){
-        $(this).attr('src', 'img/ku.jpg');
+        $(this).attr('src', 'static/ku.jpg');
       });
 
       $('#kg-univ').mouseover(function(){
-        $(this).attr('src', 'img/door.png');
+        $(this).attr('src','static/door.png');
       });
       $('#kg-univ').mouseleave(function(){
-        $(this).attr('src', 'img/kg.jpg');
+        $(this).attr('src', 'static/kg.jpg');
       });
 
       $('#du-univ').mouseover(function(){
-        $(this).attr('src', 'img/door.png');
+        $(this).attr('src','static/door.png');
       });
       $('#du-univ').mouseleave(function(){
-        $(this).attr('src', 'img/du.jpeg');
+        $(this).attr('src', 'static/du.jpeg');
       });
 
       $('#ru-univ').mouseover(function(){
-        $(this).attr('src', 'img/door.png');
+        $(this).attr('src','static/door.png');
       });
       $('#ru-univ').mouseleave(function(){
-        $(this).attr('src', 'img/ru.png');
+        $(this).attr('src', 'static/ru.png');
       });
 
 
