ee5c93b5c616cded067b7be6d0fb92c2baaf7cb3
ryo5065 <ryodai506@gmail.com>
2020-05-06 13:56:12 +0900

    modify settings.py for static

--- class_project/settings.py
+++ class_project/settings.py
@@ -126,11 +126,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR,'static']
+# STATICFILES_DIRS = [BASE_DIR,'static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/medi/'
 
+# STATICFILES_DIRS = [BASE_DIR,'static']
+
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
