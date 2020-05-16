24ca6ee24dde8529ff9b948001eddd78a3b898ea
ryo5065 <ryodai506@gmail.com>
2020-05-16 11:36:43 +0900

    spatial.html の基礎

--- class_app/urls.py
+++ class_app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import Top,clubfunc,exfunc,spatialfunc,exspfunc
+from .views import Top,clubfunc,exfunc,spatialfunc,exspfunc,zoom,postfunc
 
 
 urlpatterns = [
@@ -8,5 +8,7 @@ urlpatterns = [
     path('club/<int:pk>',clubfunc,name="club"),
     path('ex/<int:pk>',exfunc,name="explanation"),
     path('spatial/<int:pk>',spatialfunc,name="spatial"),
-    path('exsp/<str:space>/<int:pk>',exspfunc,name="exsp")
+    path('exsp/<str:space>/<int:pk>',exspfunc,name="exsp"),
+    path('zoom/<int:pk>',zoom,name="zoom"),
+    path('zoom_post/<int:pk>',postfunc,name="post"),
 ]
